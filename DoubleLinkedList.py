da19f6ff25f45bcccbba0fa2661ba6c7f5846c91
dlselrpa <103366468+iciency@users.noreply.github.com>
2023-09-06 00:08:20 +0900

    파이썬 더블링크리스트 공부중

--- DoubleLinkedList.py
+++ DoubleLinkedList.py
@@ -1,65 +1,123 @@
 class Node:
-    def __Init__(self):
-        prev = None
-        next = None
-        value = 0
+    def __init__(self):
+        self.prev = None
+        self.next = None
+        self.value = 0
 
-length = 0
 
-head = Node()
-tail = Node()
-
-head.next = tail
-tail.prev = head
-
-#경우의 수(0 미만의 수를 인덱스로 주었는가?, get의 index가 길이를 넘어 섯는가?,)
 class DoubleLinkedList:
-    #length
+    _length = 0
 
+    head = Node()
+    tail = Node()
+
+    head.next = tail
+    tail.prev = head
+    #length
     #front,back
     #insert
     #delete
     #get
     def length(self) -> int:
-        global length
-        return length
+        return self._length
         
 
-    def get(self,index)-> Node:
-        return
+    def get(self,index):
 
+        if index < 0 or index > self.length():
+            print("인덱스의 값이 유효하지 않습니다.")
+            return
+            
+        i = 0
+        ret = self.head
+        while i <= index:
+            ret = ret.next
+            i+=1
+            
+        return ret
 
     def get_front(self) -> Node:
-        return 
+        return self.head.next
 
 
     def get_back(self) -> Node:
-        return 
+        return self.tail.prev
 
 
     def insert(self,index,value) -> Node:
-        global length
-        length += 1
-        return
+        indexNode = self.get(index)
 
+        if indexNode == None:
+            return
+        
+        newnode = Node()
+        newnode.value = value
+
+        self._length += 1
+
+        if self.head.next is self.tail:
+            self.head.next = newnode
+            self.tail.prev = newnode
+            return newnode
+        
+        if indexNode.prev == None:
+            newnode.next = self.head.next
+            newnode.next.prev = newnode
+            self.head.next = newnode
+            return newnode
+        
+        newnode.next = indexNode.next
+        newnode.prev = indexNode
+        indexNode.next = newnode
+        indexNode.next.prev = newnode
+        return newnode
 
     def insert_front(self,value) -> Node:
+        self._length += 1
         return
     
 
-    def insert_back(self,value) -> Node:
-        return
-    
+    def insert_back(self,value):
+        newnode = Node()
+        newnode.value = value
+
+        if self.head.next is self.tail:
+            self.head.prev = newnode
+            self.tail.next = newnode
+
+            self._length += 1
+            return newnode
+
+        newnode.next = self.tail.prev
+        newnode.next.next = newnode
+        self.tail.prev = newnode
+
+        self._length += 1
+        return newnode
+        
 
     def delete(self,index):
+        self._length -= 1
         return
 
 
     def delete_front(self):
+        self._length -= 1
         return
     
 
     def delete_back(self):
+        self._length -= 1
         return
     
-    
+dlist = DoubleLinkedList()
+
+dlist.insert(1,1)
+dlist.insert(0,1)
+dlist.insert(1,3) 
+dlist.insert_back(4)
+
+
+print(dlist.head.next.value)
+print(dlist.head.next.next.value)
+print(dlist.head.next.next.next.value)
